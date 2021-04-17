41bdd45b469a7bfdf78674e8cda86eade5aa1e7c
warleyserafim <warleyserafim777@gmail.com>
2021-04-17 15:00:00 -0400

    update

--- snake.py
+++ snake.py
@@ -1,41 +1,42 @@
-import pygame, random
+import pygame
+import random
 from pygame.locals import *
 
-def on_grid_random():
-    x = random.randint(0,590)
-    y = random.randint(0,590)
-    return (x//10 * 10, y//10 *10)
 
-def collision (c1, c2):
+def on_grid_random():
+    x = random.randint(0, 590)
+    y = random.randint(0, 590)
+    return x // 10 * 10, y // 10 * 10
+
+
+def collision(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
+
 
 UP = 0
 RIGHT = 1
 DOWN = 2
 LEFT = 3
 
-
 pygame.init()
-screen = pygame.display.set_mode((600,600))
+screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption('Snake')
 
-snake = [(200, 200), (210,200), (220,220)]
-snake_skin = pygame.Surface((10,10))
-snake_skin.fill((255,255,125))
+snake = [(200, 200), (210, 200), (220, 220)]
+snake_skin = pygame.Surface((10, 10))
+snake_skin.fill((255, 255, 125))
 
 apple_pos = on_grid_random()
-apple = pygame.Surface((10,10))
-apple.fill((255,0,0))
+apple = pygame.image.load('maca.png')
+apple = pygame.Surface((10, 10))
+apple.fill((0, 233, 0))
 
 my_direction = LEFT
 
 clock = pygame.time.Clock()
 
-
 font = pygame.font.Font('freesansbold.ttf', 18)
 score = 0
-    
-
 
 while True:
 
@@ -45,47 +46,44 @@ while True:
         if event.type == QUIT:
             pygame.quit()
 
-
         if event.type == KEYDOWN:
             if event.key == K_UP:
                 my_direction = UP
             if event.key == K_DOWN:
                 my_direction = DOWN
             if event.key == K_LEFT:
-                my_direction = LEFT            
+                my_direction = LEFT
             if event.key == K_RIGHT:
-                my_direction = RIGHT  
+                my_direction = RIGHT
 
     if collision(snake[0], apple_pos):
         apple_pos = on_grid_random()
-        snake.append((0,0))
+        snake.append((0, 0))
         score = score + 1
 
     for i in range(len(snake) - 1, 0, -1):
-        snake[i] = (snake[i-1][0], snake[i-1][1])
+        snake[i] = (snake[i - 1][0], snake[i - 1][1])
 
     if my_direction == UP:
-        snake[0] = (snake[0][0], snake[0][1] - 10)       
+        snake[0] = (snake[0][0], snake[0][1] - 10)
 
     if my_direction == DOWN:
         snake[0] = (snake[0][0], snake[0][1] + 10)
 
     if my_direction == LEFT:
-        snake[0] = (snake[0][0] - 10, snake[0][1]) 
+        snake[0] = (snake[0][0] - 10, snake[0][1])
 
     if my_direction == RIGHT:
         snake[0] = (snake[0][0] + 10, snake[0][1])
 
-
-    screen.fill((0,0,0))   
+    screen.fill((0, 0, 0))
     screen.blit(apple, apple_pos)
     for pos in snake:
-        screen.blit(snake_skin,pos)
+        screen.blit(snake_skin, pos)
 
-    score_font = font.render('Score: %s' % (score), True, (255,255,255))
+    score_font = font.render('Score: %s' % score, True, (255, 255, 255))
     score_rect = score_font.get_rect()
     score_rect.topleft = (600 - 120, 10)
     screen.blit(score_font, score_rect)
 
-
-    pygame.display.update()        
+    pygame.display.update()
